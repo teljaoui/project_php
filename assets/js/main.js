17e8f0586df865943c1dbbe83137d961099f5a7c
teljaoui <teljaoui2004@gmail.com>
2024-10-21 11:08:46 +0100

    up

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,3 +5,5 @@ document.querySelector('#search-icon').onclick = () => {
 document.querySelector('#close').onclick = () => {
     document.querySelector('#search-form').classList.remove('active');
 }
+
+
