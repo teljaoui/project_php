b9efb051cf467c46669d26b3510f25fc0c4fb3e7
teljaoui <teljaoui2004@gmail.com>
2024-10-23 21:19:57 +0100

    up

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,3 +7,13 @@ document.querySelector('#close').onclick = () => {
 }
 
 
+
+var mainImg = document.getElementById("mainImg");
+
+var smallImg =  document.getElementsByClassName("small-img"); 
+
+for(let i=0 ; i<4 ; i++){
+    smallImg[i].onclick = function(){
+        mainImg.src = smallImg[i].src;
+    }
+}
