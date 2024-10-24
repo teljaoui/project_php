5fdb61f7abf70013ac2ac318d2e1387438460b2e
teljaoui <teljaoui2004@gmail.com>
2024-10-24 11:43:17 +0100

    up

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,10 +10,22 @@ document.querySelector('#close').onclick = () => {
 
 var mainImg = document.getElementById("mainImg");
 
-var smallImg =  document.getElementsByClassName("small-img"); 
+var smallImg = document.getElementsByClassName("small-img");
 
-for(let i=0 ; i<4 ; i++){
-    smallImg[i].onclick = function(){
+for (let i = 0; i < 4; i++) {
+    smallImg[i].onclick = function () {
         mainImg.src = smallImg[i].src;
     }
 }
+
+function showaside() {
+    var asidebar = document.getElementsByClassName('show-bar')[0];
+    asidebar.style.left = 0
+}
+
+
+function closebar() {
+    var asidebar = document.getElementsByClassName('show-bar')[0];
+    asidebar.style.left = '-100%'
+}
+
