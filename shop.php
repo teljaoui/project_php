aed494499d01ddced48d3d7626cefbf265de00e2
teljaoui <teljaoui2004@gmail.com>
2024-11-20 15:48:03 +0100

    up

--- shop.php
+++ shop.php
@@ -2,7 +2,7 @@
 include('server/connection.php');
 
 $products = null;
-$limit = 4;
+$limit = 1;
 $page = isset($_GET['page']) ? (int) $_GET['page'] : 1;
 $page = max($page, 1);
 $offset = ($page - 1) * $limit;
@@ -160,19 +160,21 @@ $stmt_total->close();
                     <ul class="pagination">
                         <li class="page-item <?php if ($page <= 1)
                             echo 'disabled'; ?>">
-                            <a class="page-link" href="?page=<?php echo $page - 1; ?>">Previous</a>
+                            <a class="page-link"
+                                href="?page=<?php echo $page - 1; ?><?php echo $category ? '&category=' . urlencode($category) : ''; ?>">Previous</a>
                         </li>
 
                         <?php for ($i = $start_page; $i <= $end_page; $i++): ?>
                             <li class="page-item">
-                                <a class="page-link  <?php echo ($i == $page) ? 'activenav' : ''; ?>"
-                                    href="?page=<?php echo $i; ?>"><?php echo $i; ?></a>
+                                <a class="page-link <?php echo ($i == $page) ? 'activenav' : ''; ?>"
+                                    href="?page=<?php echo $i; ?><?php echo $category ? '&category=' . urlencode($category) : ''; ?>"><?php echo $i; ?></a>
                             </li>
                         <?php endfor; ?>
 
                         <li class="page-item <?php if ($page >= $total_pages)
                             echo 'disabled'; ?>">
-                            <a class="page-link" href="?page=<?php echo $page + 1; ?>">Next</a>
+                            <a class="page-link"
+                                href="?page=<?php echo $page + 1; ?><?php echo $category ? '&category=' . urlencode($category) : ''; ?>">Next</a>
                         </li>
                     </ul>
                 </nav>
